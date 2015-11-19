bd0c11cc1b85e91c648662394a2e281d115e0a84
Carlton Ivy <cwivy@crimson.ua.edu>
2015-11-19 12:02:09 -0600

    Added Account and Member classes
    Early versions of acct and member class, they will likely change
    frequently

---
+++ Account.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS220GroupProject
+{
+    class Account
+    {
+        private int id;
+        private int pass;
+        private string fName;
+        private string lName;
+        private string address;
+        private string phone;
+        private string city;
+        private string state;
+        private int zip;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Pass
+        {
+            get { return pass; }
+            set { pass = value; }
+        }
+
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public int Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS220GroupProject
+{
+    class Member : Account
+    {
+        private int cardNum;
+
+        private double balance;
+
+        private List<int> rentalsHeld;
+
+
+        public int CardNum
+        {
+            get { return cardNum; }
+            set { cardNum = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public List<int> RentalsHeld
+        {
+            get { return rentalsHeld; }
+            set { rentalsHeld = value; }
+        }
+    }
+}
