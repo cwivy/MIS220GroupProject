1a88fdca609ddf98830279578b88fd2b2c73e99c
laday <laday@crimson.ua.edu>
2015-11-19 19:11:20 -0600

    CreateAccount

--- AccountDL.cs
+++ AccountDL.cs
@@ -22,7 +22,10 @@ namespace MIS220GroupProject
         public static void CreateAccount(string fName, string lName, string address1, string address2, string phone, string city, string state, string zip, DateTime dateOfBirth)
         {
             string sqlText;
-            sqlText = "EmployeeInsert";
+
+
+            sqlText = "INSERT INTO Member(FirstName, LastName, DOB, Address1, Address2, City, State, Zip, Phone)";
+            sqlText += "VALUES(@firstName, @lastName, @DOB, @address1, @address2, @city, @state, @zip, @phone)";
 
             SqlConnection connection = new SqlConnection(connectionString);
 
