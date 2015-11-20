4d347f69534983058a172def3e08221e6066c480
cagetsinger <cagetsinger@crimson.ua.edu>
2015-11-19 18:21:12 -0600

    nothing

--- TestForm.Designer.cs
+++ TestForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 253);
+            this.Name = "TestForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
