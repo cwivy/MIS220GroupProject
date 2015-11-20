059262a42050b0040ef91f23176215f85cbdc30d
Carlton Ivy <cwivy@crimson.ua.edu>
2015-11-19 18:17:37 -0600

    Idk what these are really but I'm doing it

--- TestForm.Designer.cs
+++ TestForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 287);
+            this.Name = "TestForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
