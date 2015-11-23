cbfe0c4aafb30b0f58150632cacd018d0798fdca
cagetsinger <cagetsinger@crimson.ua.edu>
2015-11-23 15:53:28 -0600

    newMemForm

--- NewMemForm.cs
+++ NewMemForm.cs
@@ -39,24 +39,41 @@ namespace MIS220GroupProject
             thisMember.Phone = Convert.ToString(phoneNumBox);
         }
 
+        private void ClearTextBoxes()
+        {
+            firstNameBox.Text = " ";
+            lastNameBox.Text = " ";
+            DOBDateTimePicker.Value = DateTime.Now;
+            address1Box.Text = " ";
+            address2Box.Text = " ";
+            cityBox.Text = " ";
+            stateBox.Text = " ";
+            zipBox.Text = " ";
+            phoneNumBox.Text = " ";
+        }
+
         //--------Event Handlers-----------
         private void saveAccInfoButton_Click(object sender, EventArgs e)
         {
-            CopyFormToObject();
-            //string zipString;
 
+            thisMember = new Member();
 
-            //thisMember.FName = firstNameBox.Text.ToString();
-            //thisMember.LName = lastNameBox.Text.ToString();
-            //thisMember.DateOfBirth = Convert.ToDateTime(DOBDateTimePicker.Text);
-            //thisMember.Address1 = address1Box.Text.ToString();
-            //thisMember.Address2 = address2Box.Text.ToString();
-            //thisMember.City = cityBox.Text.ToString();
-            //thisMember.State = stateBox.Text.ToString();
-            //thisMember.Zip = Convert.ToInt32(zipBox.Text);
-            //thisMember.Phone = Convert.ToString(phoneNumBox);
+            thisMember.FName = firstNameBox.Text;
+            thisMember.LName = lastNameBox.Text;
+            thisMember.DateOfBirth = Convert.ToDateTime(DOBDateTimePicker.Text);
+            thisMember.Address1 = address1Box.Text;
+            thisMember.Address2 = address2Box.Text;
+            thisMember.City = cityBox.Text;
+            thisMember.State = stateBox.Text;
+            thisMember.Zip = Convert.ToInt32(zipBox.Text);
+            thisMember.Phone = Convert.ToString(phoneNumBox.Text);
 
-            thisMember.CreateAccount();
+            string DOBString = Convert.ToString(thisMember.DateOfBirth);
+            DOBString = DOBString.Substring(0, 10);
+
+            thisMember.CreateAccount(thisMember.FName, thisMember.LName, thisMember.Address1, thisMember.Address2, thisMember.Phone, thisMember.City, thisMember.State, thisMember.Zip, DOBString);
+            ClearTextBoxes();
+            MessageBox.Show("Thank you! Your member account has been updated");
         }
     }
 }
